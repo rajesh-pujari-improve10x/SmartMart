9cad4f2f65ce85f1b3e0a967e4d5cb25db022b29
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-06-01 22:44:14 +0530

    I Created SplashActivity, CategoriesActivity and Implemented the SplashActivity functionality

---
+++ app/src/main/java/com/improve10x/smartmart/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.improve10x.smartmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.improve10x.smartmart.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, CategoriesActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+    }
+}
