2aec65076b19c62414cae1df897ae560db22870a
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-06-05 10:18:52 +0530

    I implemented Cart functionality in ProductsActivity and ProductDetailsActivity

--- app/src/main/java/com/improve10x/smartmart/cart/CartProductsActivity.java
+++ app/src/main/java/com/improve10x/smartmart/cart/CartProductsActivity.java
@@ -33,26 +33,6 @@ public class CartProductsActivity extends BaseActivity {
         setupCartProductsRv();
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.messages_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.add) {
-            Intent intent = new Intent(this, AddMessageActivity.class);
-            startActivity(intent);
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-    */
-
-
     private void fetchCartProducts() {
         showProgressBar();
         Call<Cart> call = fakeAPiServices.fetchCartProducts();
